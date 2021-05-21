7f1dc55ee5377a27860595c58b56b4e351032936
bappyasif <bappyasif@hotmail.com>
2021-05-22 03:57:56 +0600

    Added to previous commit with trying out chor code and charCodeFrom methods of String object to solve other kind of text cases fro appendAndDelete exercise.

--- moreFromHackerRank/appendDelete/index.js
+++ moreFromHackerRank/appendDelete/index.js
@@ -10,6 +10,23 @@ function appendDelete(s, t, k) {
   // console.log(temp);
   // checking rules
   if (temp.length > k) return "No";
+  // if (t.length - temp.length <= k) {
+  //   return "Yes";
+  // }
+  if(s.length === t.length) {
+    let temp2 = ""
+    for(let i=0; i<s.length; i++) {
+      // console.log(s.charAt(i+t.length), String.fromCharCode(s[i+t.length]) === t[i], String.fromCharCode(s.charCodeAt(i+t.length)))
+      
+      if(String.fromCharCode(s.charCodeAt(i+t.length)) === t[i]) {
+        temp2 += s[i];
+      } else {
+        break
+      }
+    }
+    return temp2 === t;
+  }
+
   if (t.length - temp.length <= k) {
     return "Yes";
   }
@@ -19,9 +36,10 @@ function appendDelete(s, t, k) {
   //   console.log(temp);
 }
 
-output = appendDelete("ashley", "ash", 2);
-output = appendDelete("aba", "aba", 7);
+// output = appendDelete("ashley", "ash", 2);
+// output = appendDelete("aba", "aba", 7);
 // output = appendDelete("hackerhappy", "hackerrank", 9);
+output = appendDelete("abc", "def", 6);
 console.log(output);
 
 /**
