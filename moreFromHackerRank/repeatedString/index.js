9ff9d47fed3e512b3c4f52924b1361ba31e2a0bd
bappyasif <bappyasif@hotmail.com>
2021-06-21 03:10:55 +0600

    Added to previous commit now fully test case passable code for previously attempted code for this repeated string execise.

--- moreFromHackerRank/repeatedString/index.js
+++ moreFromHackerRank/repeatedString/index.js
@@ -1,4 +1,77 @@
 function repeatedString(str, n) {
+    let countA = 0;
+    let countMultiple = 0;
+
+    let size = str.length;
+    let repeated = Math.floor(n/size);
+    console.log(size, repeated, "??");
+
+
+    let i = str.length;
+    while(i >= 0) {
+        // console.log(str[i]);
+        if(str[i] == 'a') countA++;
+        i--;
+    }
+    console.log(countA, countA*countMultiple, countA*repeated);
+    // countA = countA*countMultiple
+    countA = countA*repeated;
+
+    let rems = n%str.length
+    console.log(rems);
+    do {
+        rems--;
+        if(str[rems] == 'a') {
+            countA++;
+        }
+    } while(rems > 0)
+    console.log(countA, "::")
+}
+
+// output = repeatedString('abcac', 10);
+// output = repeatedString('abcac', 12);
+output = repeatedString('aba', 10);
+console.log(output);
+
+/**
+ * 
+ * 
+ function repeatedString(str, n) {
+    let countA = 0;
+    let countMultiple = 0;
+
+    for(let i=0; i<n; i+=str.length) {
+        if(i+str.length > n) {
+            break;
+        } else {
+            countMultiple++;
+        }
+        // console.log(countMultiple)
+    }
+    console.log(countMultiple)
+
+    let i = str.length;
+    while(i >= 0) {
+        // console.log(str[i]);
+        if(str[i] == 'a') countA++;
+        i--;
+    }
+    console.log(countA, countA*countMultiple);
+    countA = countA*countMultiple
+
+    let rems = n%str.length
+    console.log(rems);
+    do {
+        rems--;
+        if(str[rems] == 'a') {
+            countA++;
+        }
+    } while(rems > 0)
+    console.log(countA, "::")
+}
+ * 
+ * 
+ function repeatedString(str, n) {
     let checkStr = str.length % n;
     console.log(checkStr);
     let seq = '';
@@ -18,8 +91,4 @@ function repeatedString(str, n) {
     }
     return seq.split('').filter(v=>v=='a').length;
 }
-
-// output = repeatedString('abcac', 10);
-// output = repeatedString('abcac', 12);
-output = repeatedString('aba', 10);
-console.log(output);
+ */
