cd2f9a0207d62f71c823dcf3b954330d8a62634c
bappyasif <bappyasif@hotmail.com>
2021-06-27 03:38:38 +0600

    Added to previous commit with a full test case coveraged solution for equalizeArray exercise from hacker rank.

--- moreFromHackerRank/equalizeArray/index.js
+++ moreFromHackerRank/equalizeArray/index.js
@@ -1,4 +1,46 @@
 function equalizeArray(arr) {
+    let hash = {};
+    for(let i=0; i<arr.length; i++) {
+        hash[arr[i]] = hash[arr[i]] ? hash[arr[i]]+1 : 1
+    }
+    let values = Object.values(hash);
+    let max = Math.max(...values);
+    console.log(hash, max)
+    let filtered = values.filter(v=>v!=max);
+    console.log(filtered);
+    // return filtered.length >= max ? filtered.length : max;
+    return arr.length - max
+}
+
+output = equalizeArray([1,2,2,3]);
+// output = equalizeArray([3,3,2,1,3]);
+// output = equalizeArray([1,2,3,1,2,3,3,3]);
+console.log(output)
+
+/**
+ * 
+ * 
+ function equalizeArray(arr) {
+    let filtered = [];
+    let hash = {};
+    for(let i=0; i<arr.length; i++) {
+        if(arr[i] == arr[i+1] || arr[i] == arr[i-1] || filtered.includes(arr[i])) {
+            filtered.push(arr[i]);
+        }
+        hash[arr[i]] = hash[arr[i]] ? hash[arr[i]]+1 : 1
+    }
+    let values = Object.values(hash);
+    let max = Math.max(...values);
+    console.log(hash, max)
+    return filtered
+
+    // return arr.length - filtered.length
+
+    // return filtered.length;
+}
+ * 
+ * 
+ function equalizeArray(arr) {
     let filtered = [];
     for(let i=arr.length -1; i>=0; i--) {
         if(arr[i] == arr[i+1] || arr[i] == arr[i-1] || filtered.includes(arr[i])) {
@@ -23,13 +65,6 @@ function equalizeArray(arr) {
 
     // return filtered.length;
 }
-
-// output = equalizeArray([1,2,2,3]);
-// output = equalizeArray([3,3,2,1,3]);
-output = equalizeArray([1,2,3,1,2,3,3,3]);
-console.log(output)
-
-/**
  * 
  * 
  function equalizeArray(arr) {
